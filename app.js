2dffefd78b3cef03033a56dde7cfaa8b8a6be996
bladimir doumerc <bdoumerc15@gmail.com>
2021-01-05 13:20:16 -0700

    Se ha agregado EJS package a nuestro proyecto del to do list y mejoras en el apartado index.js

--- app.js
+++ app.js
@@ -1,10 +1,17 @@
+//jshint esversion:6
+
 import express from 'express'
 import bodyParser from 'body-parser'
 
 const app = express();
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static('public'));
 
 app.get('/', (req, res) => {
-    res.send('<h1>Hello World</h1>');
+    const cwd = process.cwd();
+    const file = 'index.html';
+    const pathFile = `${cwd}\\${file}`;
+    res.sendFile(pathFile);
 })
 
 app.listen(3000, () => console.log('Server listening on Port 3000'));
