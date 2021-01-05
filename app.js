614f17c81ff5773047306e0a30e6bc931d7beeec
bladimir doumerc <bdoumerc15@gmail.com>
2021-01-05 16:51:16 -0700

    Se mejoraron errores y se eliminaron cosas que no se ocupaban

--- app.js
+++ app.js
@@ -2,30 +2,14 @@
 
 import express from 'express'
 import bodyParser from 'body-parser'
+import { day } from "./public/index.js";
 
-const date = new Date();
 const app = express();
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static('public'));
 
 app.get('/', (req, res) => {
-    // const cwd = process.cwd();
-    // const file = 'index.html';
-    // const pathFile = `${cwd}\\${file}`;
-    // res.sendFile(pathFile);
-    // const cwd = process.cwd();
-    const weekdays = [
-        { name: 'domingo', number: 0, weekend: true, workDay: false },
-        { name: 'lunes', number: 1, weekend: false, workDay: true },
-        { name: 'martes', number: 2, weekend: false, workDay: true },
-        { name: 'miércoles', number: 3, weekend: false, workDay: true },
-        { name: 'jueves', number: 4, weekend: false, workDay: true },
-        { name: 'viernes', number: 5, weekend: false, workDay: true },
-        { name: 'sábado', number: 6, weekend: true, workDay: false },
-    ];
-    const weekday = date.getDay();
-    const day = weekdays[weekday].name;
     res.render('list', { ejsDay: day });
 })
 
--- public/index.js
+++ public/index.js
@@ -1,31 +1,58 @@
-const date = new Date()
+// const date = new Date()
 
-const newYearsEve = function() {
-    // let message = 'Today is New Years Eve';
-    // if (date.getDate() === 31 && date.getMonth() === 11)
-    //     return message
-    // message = 'Today is not New Years Eve';
-    // return message;
+// const newYearsEve = function() {
+//     // let message = 'Today is New Years Eve';
+//     // if (date.getDate() === 31 && date.getMonth() === 11)
+//     //     return message
+//     // message = 'Today is not New Years Eve';
+//     // return message;
 
-    const weekdays = [
-        { name: 'sunday', number: 0, weekend: true, workDay: false },
-        { name: 'monday', number: 1, weekend: false, workDay: true },
-        { name: 'tuesday', number: 2, weekend: false, workDay: true },
-        { name: 'wednesday', number: 3, weekend: false, workDay: true },
-        { name: 'thursday', number: 4, weekend: false, workDay: true },
-        { name: 'friday', number: 5, weekend: false, workDay: true },
-        { name: 'saturday', number: 6, weekend: true, workDay: false },
-    ];
+//     const weekdays = [
+//         { name: 'sunday', number: 0, weekend: true, workDay: false },
+//         { name: 'monday', number: 1, weekend: false, workDay: true },
+//         { name: 'tuesday', number: 2, weekend: false, workDay: true },
+//         { name: 'wednesday', number: 3, weekend: false, workDay: true },
+//         { name: 'thursday', number: 4, weekend: false, workDay: true },
+//         { name: 'friday', number: 5, weekend: false, workDay: true },
+//         { name: 'saturday', number: 6, weekend: true, workDay: false },
+//     ];
 
-    return `Today is ${weekdays[date.getDay()].name}`;
+//     return `Today is ${weekdays[date.getDay()].name}`;
 
+// }
+
+// export const day = newYearsEve();
+
+
+// // const answer = document.createElement('h1');
+// // answer.textContent = newYearsEve();
+// // document.body.append(answer.textContent);
+
+// // document.body.appendChild(answer);
+
+// const date = new Date();
+// const options = {
+//     weekday: 'long',
+//     year: 'numeric',
+//     month: 'long',
+//     day: 'numeric'
+// }
+
+// const local = 'es-MX';
+
+// export const day = date.toLocaleDateString(local, options);
+
+const date = new Date();
+const localDate = function() {
+    const options = {
+        weekday: 'long',
+        year: 'numeric',
+        month: 'long',
+        day: 'numeric'
+    }
+    const local = 'es-MX';
+    return date.toLocaleDateString(local, options);
 }
 
-const day = newYearsEve();
 
-
-// const answer = document.createElement('h1');
-// answer.textContent = newYearsEve();
-// document.body.append(answer.textContent);
-
-// document.body.appendChild(answer);
+export const day = localDate();
